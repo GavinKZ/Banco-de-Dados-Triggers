633665fd1ead08a3ed468f81efac5562feec2976
GavinKZ <144485700+GavinKZ@users.noreply.github.com>
2023-10-24 23:02:19 -0300

    Ex 3

--- exerciciosDeFixacao20231024.sql
+++ exerciciosDeFixacao20231024.sql
@@ -5,3 +5,18 @@ CREATE TRIGGER novo_cliente AFTER INSERT ON Clientes
 -- 2.
 CREATE TRIGGER exclusao_cliente BEFORE DELETE ON Clientes
 	FOR EACH ROW INSERT INTO Auditoria VALUES("Houve uma tentativa de exclusão de um cliente");
+
+-- 3.
+DELIMITER //
+CREATE TRIGGER att_cliente AFTER UPDATE ON Clientes
+	FOR EACH ROW
+	BEGIN
+		DECLARE nome_antigo VARCHAR(255);
+		DECLARE nome_novo VARCHAR(255);
+
+		SET nome_antigo = OLD.nome;
+		SET nome_novo = NEW.nome;
+
+		INSERT INTO Auditoria VALUES(nome_antigo, nome_novo);
+	END //
+DELIMITER ;
